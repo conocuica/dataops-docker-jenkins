bb63b55b33af955e90d70aa1eb278b8f4f847093
Conocuica <conocuica@gmail.com>
2026-02-15 19:41:39 -0500

    Creado Salida 5

--- app/main.py
+++ app/main.py
@@ -6,10 +6,14 @@ data = {
 }
 
 df = pd.DataFrame(data)
-df["Comision"] = df["Ventas"] * 0.17
+df["Comision"] = df["Ventas"] * 0.16
 
-df.to_excel("comisiones.xlsx", index=False)
-#df.to_excel("/workspace/comisiones.xlsx", index=False)
+# Crear carpeta output si no existe
+os.makedirs("/app/output", exist_ok=True)
+
+df.to_excel("/app/output/comisiones.xlsx", index=False)
+
+#df.to_excel("comisiones.xlsx", index=False)
 
 print("Archivo generado correctamente")
     
